abba6989557ad3305224498e9f18f3b91134372d
Lobsang <lobsangmercedes@gmail.com>
2023-04-26 20:45:31 -0400

    Product Card and integration in home page

---
+++ src/components/productCard.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="card shadow-lg max-w-xs rounded overflow-hidden">
+      <img :src="image" :alt="title" class="w-full">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">{{ title }}</div>
+        <p class="text-gray-700 text-base">{{ description }}</p>
+        <p class="text-gray-700 text-base">{{ price }}</p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <button class="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full" @click="addToCart()">Add to Cart</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: 'Product Title'
+      },
+      image: {
+        type: String,
+        default: 'https://via.placeholder.com/300x200'
+      },
+      description: {
+        type: String,
+        default: 'Some text about the product...'
+      },
+      price: {
+        type: String,
+        default: '$0.00'
+      }
+    },
+    methods: {
+      addToCart() {
+        // aquí se puede agregar la lógica para agregar el producto al carrito
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .card {
+    background-color: #fff;
+  }
+  
+  .card img {
+    object-fit: cover;
+    object-position: center;
+    height: 200px;
+  }
+  
+  .card button {
+    width: 100%;
+  }
+  
+  </style>
--- src/pages/Store/homePageEcommerce.vue
+++ src/pages/Store/homePageEcommerce.vue
@@ -1,93 +1,104 @@
 <template>
-    <div class="bg-light-gray">
-      <TopBar />
-      <div class="d-flex flex-row justify-content-between margin-top">
-        <div class="flex-1 mr-4">
-          <realtimelastbuy class="flex-1 realtimelastbuy" />
-        </div>
-        <div class="flex-1">
-          <realtimenewproduct class="flex-1 realtimenewproduct" />
-        </div>
-        <div class="flex-1">
-          <realtimenewproduct class="flex-1 realtimenewproduct" />
-        </div>
-        
+  <div class="bg-light-gray">
+    <TopBar />
+    <div class="d-flex flex-row justify-content-between margin-top">
+      <div class="flex-1 mr-4">
+        <table class="table-fixed w-2/5">
+          <realtimelastbuy />
+        </table>
+      </div>
+      <div class="flex-1">
+        <newsbox />
+      </div>
+      <div class="flex-1">
+        <table class="table-fixed w-2/5">
+          <realtimenewproduct />
+        </table>
       </div>
     </div>
-  </template>
+    <productCard/>
+    
+  </div>
+</template>
+
   
-  <script>
-  import TopBar from '@/components/headerNav.vue';
-  import realtimelastbuy from '@/components/realtimelastbuy.vue';
-  import realtimenewproduct from '@/components/realtimenewproduct.vue';
-  
-  export default {
-    name: 'HomePageEcommerce',
-    components: {
-      TopBar,
-      realtimelastbuy,
-      realtimenewproduct,
-    },
-    data() {
-      return {
-        product1: {
-          title: 'Backpack',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit In odit exercitationem fuga id nam quia',
-          image: 'https://images.unsplash.com/photo-1494726161322-5360d4d0eeae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
-          rating: 3,
-          price: 220,
-        },
-        product2: {
-          title: 'Sneakers',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit In odit exercitationem fuga id nam quia',
-          image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
-          rating: 4,
-          price: 120,
-        },
-        product3: {
-          title: 'Watch',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit In odit exercitationem fuga id nam quia',
-          image: 'https://images.unsplash.com/photo-1521737711862-5a810fda9665?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
-          rating: 5,
-          price: 300,
-        },
-      };
-    },
-    methods: {
-      addToCart(product) {
-        console.log('Product added to cart:', product);
-        // Implementar la lógica para agregar el producto al carrito aquí
+<script>
+import TopBar from '@/components/headerNav.vue';
+import realtimelastbuy from '@/components/realtimelastbuy.vue';
+import realtimenewproduct from '@/components/realtimenewproduct.vue';
+import newsbox from '@/components/newsbox.vue';
+import productCard from '@/components/productCard.vue';
+
+
+export default {
+  name: 'HomePageEcommerce',
+  components: {
+    TopBar,
+    realtimelastbuy,
+    realtimenewproduct,
+    newsbox,
+    productCard,
+  },
+  data() {
+    return {
+      product1: {
+        title: 'Backpack',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit In odit exercitationem fuga id nam quia',
+        image: 'https://images.unsplash.com/photo-1494726161322-5360d4d0eeae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+        rating: 3,
+        price: 220,
       },
+      product2: {
+        title: 'Sneakers',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit In odit exercitationem fuga id nam quia',
+        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+        rating: 4,
+        price: 120,
+      },
+      product3: {
+        title: 'Watch',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit In odit exercitationem fuga id nam quia',
+        image: 'https://images.unsplash.com/photo-1521737711862-5a810fda9665?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+        rating: 5,
+        price: 300,
+      },
+    };
+  },
+  methods: {
+    addToCart(product) {
+      console.log('Product added to cart:', product);
+      // Implementar la lógica para agregar el producto al carrito aquí
     },
-  };
-  </script>
+  },
+};
+</script>
   
-  <style>
-  .bg-light-gray {
-    background-color: #f2f2f2;
-  }
-  
-  .d-flex {
-    display: flex;
-  }
-  
-  .flex-row {
-    flex-direction: row;
-  }
-  
-  .justify-content-between {
-    justify-content: space-between;
-  }
-  
-  .realtimelastbuy {
-    margin-right: 50px;
-  }
-  
-  .realtimenewproduct {
-    margin-left: 100px;
-  }
-  
-  .margin-top {
-    margin-top: 16px;
-  }
-  </style>
+<style>
+.bg-light-gray {
+  background-color: #f2f2f2;
+}
+
+.d-flex {
+  display: flex;
+}
+
+.flex-row {
+  flex-direction: row;
+}
+
+.justify-content-between {
+  justify-content: space-between;
+}
+
+.realtimelastbuy {
+  margin-right: 50px;
+}
+
+.realtimenewproduct {
+  margin-left: 100px;
+}
+
+.margin-top {
+  margin-top: 16px;
+}
+</style>
